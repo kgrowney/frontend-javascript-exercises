6e2b81a52dc4fa5cc4f677b03b0ad7e6bd794985
vagrant <vagrant@precise32.(none)>
2014-10-03 13:00:46 +0000

    Booleans Assignment

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,22 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+//true if the same
+return (stringOne == stringTwo);
 };
 
 module.exports.notEqual = function(one, two) {
-
+//false if same type and equal 
+return (one !== two);
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+//true if middle is inBetween lower and upper  
+return ((lower < middle) && (middle < upper));
 };
 
 module.exports.outsideRanges = function(number) {
-
+ return(!(number >= 10 && number <= 20) && !(number > 42 && number <= 75) && !(number > 1 && number < 6));
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  return ((name == 'NYTimes' || name == 'LATimes') && (price >= 1));
 };
