7685b2618d852d82742f01f24a7eb798ab6d2c58
vagrant <vagrant@precise32.(none)>
2014-10-09 20:30:04 +0000

    Objects Exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,34 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+		var courseObj = new Object ();
+		courseObj.title = courseTitle;
+		courseObj.duration = courseDuration;
+		courseObj.students = courseStudents; 
 
-module.exports.addProperty = undefined;
+		return courseObj;
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(propObject, propNewProp, propNewValue) {
+		if (propObject.propNewProp === undefined) {
+			propObject[propNewProp] = propNewValue;
+			return propObject;
+		}
+		else {
+			return propObject;
+		};
+	};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(letter) {
+	//Combine the three properties into string + add text
+	return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender
+};
+
+
+module.exports.canIGet = function(item, money) { 
+	var Object = {};
+	Object['MacBook Air'] = 999;
+	Object['MacBook Pro'] = 1299;
+	Object['Mac Pro'] = 2499;
+	Object['Apple Sticker'] = 1;
+
+	return (money >= Object[item]);
+};
